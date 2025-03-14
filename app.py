874094aba8ce9a2f37a3ec37281ed0e7b25c4874
jgooden196 <jgooden196@gmail.com>
2025-03-14 12:56:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -69,4 +69,5 @@ def status():
     return jsonify(status_info)
 
 if __name__ == '__main__':
-    app.run(debug=config.DEBUG, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
